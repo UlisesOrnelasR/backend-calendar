e86f7773a40e26fe8fc4eed3b2f4e4bcd2e49929
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-25 16:13:02 -0700

    feat: Create an user in Database, folder models

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,25 @@
 const { response } = require("express");
+const User = require("../models/User");
 
-const createUser = (req, res = response) => {
-  const { name, email, password } = req.body;
-  res.status(201).json({
-    ok: true,
-    msg: "register",
-    name,
-    email,
-    password,
-  });
+const createUser = async (req, res = response) => {
+  // const { name, email, password } = req.body;
+
+  try {
+    const user = new User(req.body);
+
+    await user.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: "register",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Please talk to the administrator",
+    });
+  }
 };
 
 const userLogin = (req, res = response) => {
