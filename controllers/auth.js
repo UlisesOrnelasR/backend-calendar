bf35c93f7b4a4d4ae6ff746149cf17af058d0ae8
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-25 11:48:25 -0700

    chore: Retrieve information from a post

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,36 @@
 const { response } = require("express");
 
 const createUser = (req, res = response) => {
+  const { name, email, password } = req.body;
+
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: "The name must be 5 letters",
+    });
+  }
+
   res.json({
     ok: true,
     msg: "register",
+    name,
+    email,
+    password,
   });
 };
 
-const userLogin = (req, res) => {
+const userLogin = (req, res = response) => {
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
     msg: "login",
+    email,
+    password,
   });
 };
 
-const revalidateToken = (req, res) => {
+const revalidateToken = (req, res = response) => {
   res.json({
     ok: true,
     msg: "renew",
