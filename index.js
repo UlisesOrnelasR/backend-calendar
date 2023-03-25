e7dda6ce23ddeb46b119ffb43cb322d761ad3e21
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-25 13:43:37 -0700

    feat: Connecting Node to Mongo Atlas Database

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
-require("dotenv").config();
+require("dotenv").config(); // Carga las variables de entorno del archivo .env y unicamente es requerido llamarse una vez en el proyecto (index.js) para que esten disponibles en toda la aplicacion
+const { dbConnection } = require("./database/config");
 
 // console.log(process.env); // Imprime las variables de entorno
 
 // Crear el servidor de express
 const app = express();
+
+// Database
+dbConnection();
 
 // Directorio public
 app.use(express.static("public"));
