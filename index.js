a7da538e2ed3ca0d49e551700f984ab478ef7502
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-24 23:14:24 -0700

    chore: Settings Express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hola mundo!");
+const express = require("express");
+
+// Crear el servidor de express
+const app = express();
+
+//Rutas
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+// Escuchar peticiones
+app.listen(4000, () => {
+  console.log(`Servidor corriendo ${4000} `);
+});
